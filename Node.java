0743d732fe5d3155f496260ece414a9d83703a68
philip von mueffling <philip.vm3@gmail.com>
2021-01-06 19:29:43 -0500

    gets

---
+++ Node.java
@@ -0,0 +1,15 @@
+public class Node{
+ public Node(String value){}
+ private String data;
+ private Node next,prev;
+  public String getData(){
+    return data;
+  }
+  public String getNext(){
+    return next;
+  }
+  public String getPrev(){
+    return prev;
+  }
+ //write get/set methods for all three instance variables.
+}
