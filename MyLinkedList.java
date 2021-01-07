b3089db8d18b8b7d2f8f96dbe4b6682d27c05232
philip von mueffling <philip.vm3@gmail.com>
2021-01-06 20:04:49 -0500

    add

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,12 +1,39 @@
 public class MyLinkedList{
- private int size;
- private Node start,end;
- public MyLinkedList(){/*create a constructor*/}
- public int size();
- public boolean add(String value);
- public void add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
- //Any helper method that returns a Node object MUST BE PRIVATE!
+  private int size;
+  private Node start,end;
+  public MyLinkedList(){
+    size=0;
+    start=null;
+    end=null;
+  }
+  public int size(){
+    return size;
+  }
+  public boolean add(String value){
+    if (end==null){
+      start= new Node(value);
+      end=start;
+      size++;
+    }
+    else{
+      Node temp=new Node(value);
+      end.setNext(temp);
+      temp.setPrev(end);
+      size++;
+      end=temp;
+    }
+  }
+  public void add(int index, String value){
+
+  }
+  public String get(int index){
+
+  }
+  public String set(int index, String value){
+
+  }
+  public String toString(){
+
+  }
+  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
